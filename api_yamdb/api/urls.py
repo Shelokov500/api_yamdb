fa7bea4424c8f10e73aac1249159e2eb284fdb35
Иван Сабанаев <sabanaev424@mail.rugit config --global user.name >
2022-07-18 08:12:19 +0300

    Отправляю свои файлы

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+<<<<<<< HEAD
 from rest_framework.routers import DefaultRouter
 
 from .views import GenreViewSet, CategoryViewSet, TitleViewSet
@@ -11,3 +12,21 @@ router_v1.register('genre', GenreViewSet, basename='genre')
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
 ]
+=======
+from rest_framework import routers
+
+from .views import CommentViewSet, ReviewViewSet
+
+router_v1 = routers.DefaultRouter()
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='reviews'
+)
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet, basename='comments'
+)
+
+urlpatterns = [
+    path('v1/', include(router_v1.urls)),
+]
+>>>>>>> e40479bb661feed86a5080e9b1cb25aee9cc36d3
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from rest_framework import mixins, viewsets
 from rest_framework.filters import SearchFilter
 from reviews.models import Category, Genre, Title
@@ -41,3 +42,40 @@ class GenreViewSet(viewsets.ModelViewSet):
     # permission_classes =
     filter_backends = (SearchFilter,)
     search_fields = ('name',)
+=======
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+
+from reviews.models import Category, Genre, Review, Title, User
+from .serializers import ReviewSerializer, CommentSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        author = self.request.user
+        serializer.save(author=author, title=title)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, pk=review_id, title__id=title_id)
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, pk=review_id, title__id=title_id)
+        serializer.save(author=self.request.user, review=review)
+>>>>>>> e40479bb661feed86a5080e9b1cb25aee9cc36d3
